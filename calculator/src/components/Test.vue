78c3ca0a14923e00654a5e1b47e9509f8727cb44
tomotsune <tomotsune@foxmail.com>
2021-05-26 10:35:40 +0800

    BUG fix: 多个0的省略

--- calculator/src/components/Test.vue
+++ calculator/src/components/Test.vue
@@ -65,14 +65,12 @@ const verifyPhoneNum = () => {
 }
 let amount = ref('')
 const transform = () => {
-  console.log(amount.value)
   let unit = ["", "拾", "佰", "仟", "万", "拾万", "百万", "仟万", "亿", "拾亿", "百亿", "仟亿"]
   let capital = ["零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖"]
 
   let reg = /^([1-9]\d{0,12}|0)([.]?|(\.\d{1,2})?)$/
-  if (!reg.test(amount.value)) amount = ''
+  if (!reg.test(amount.value)) amount.value = ''
   else {
-    console.log("enter else sentence")
     let result = ''
     if (amount.value.indexOf('.') !== -1) {
       var sub = amount.value.split('.');
@@ -84,20 +82,33 @@ const transform = () => {
       result += unit[sub[1][0] - 1] + 角
       result += unit[sub[1][1] - 1] + 分
     } else {
-      // input >=3 err
       for (let i = 0; i < amount.value.length; i++) {
-        console.log("i=", i)
-        if (i + 1 !== amount.value.length || amount.value[i] !== '0') {
+        result += capital[(amount.value[i])]
+        result += unit[amount.value.length - 1 - i]
+        if (amount.value[i + 1] === '0') {
+          for (let j = i+1; j < amount.value.length; j++) {
+            console.log("into loop")
+            if (amount.value[j] !== '0') {
+              // 壹仟零零佰元整
+              result += capital[0]
+              console.log("j", j)
+              i = j;
+              break
+            } else i = j + 1
+          }
+        }
+
+
+        /*if (i + 1 !== amount.value.length || amount.value[i] !== '0') {
           result += capital[(amount.value[i])]
           result += unit[amount.value.length - 1 - i]
-        }
+        }*/
       }
     }
-    console.log(result);
     result += "元整"
+    amount.value = result
   }
-  amount.value = result
-}
+
 
 }
 </script>
